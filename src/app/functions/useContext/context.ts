34f636d2a4c0e50d91f92f527c776fb1427f7882
rdian <rananda.d.ilham@gmail.com>
2024-09-24 19:50:14 +0700

    add note

--- src/app/functions/useContext/context.ts
+++ src/app/functions/useContext/context.ts
@@ -1,11 +1,14 @@
 import { createContext, useContext } from "react";
 import { User } from "./types";
 
+// set type with undefined in case someone forgot to wrap component with DashboardContext provider
 export const DashboardContext = createContext<User | undefined>(undefined);
 
+// 
 export function useUserContext() {
     const user = useContext(DashboardContext);
 
+    // validate and throw error when user is undefined, instead of giving nulish operation and show no error at all
     if (user === undefined) {
         throw new Error("useUserContext must be used with a DashboardContext")
     }
